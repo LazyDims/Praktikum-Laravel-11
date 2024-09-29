e254d2837f9163cefa8fd3882df2787bf04b03f9
DimasArjuna <arjunadimas832@gmail.com>
2024-09-30 02:59:30 +0700

    Penambahan page mahasiswa dan home

--- resources/views/mahasiswa/search.blade.php
+++ resources/views/mahasiswa/search.blade.php
@@ -1,41 +1,37 @@
 <x-layout>
+    <x-navbar></x-navbar>
     <h1 class="text-3xl flex justify-center py-5 mx-5">
-        Data Mahasiswa
+        Pencarian Mahasiswa
     </h1>
-    <x-search></x-search>
     <x-layout-table>
         <thead>
             <tr
-                class="text-md font-semibold tracking-wide text-left text-gray-900 bg-gray-100 capitalize  border-gray-600">
+                class="text-md font-semibold tracking-wide text-left text-gray-900 bg-gray-100 capitalize border-gray-600">
                 <th class="px-4 py-3">No</th>
-                <th class="px-4 py-3">NIM</th>
+                <th class="px-4 py-3">NIK</th>
                 <th class="px-4 py-3">Nama Mahasiswa</th>
-                <th class="px-4 py-3">Fakultas / Progdi</th>
+                <th class="px-4 py-3">Tempat Tanggal Lahir</th>
                 <th class="px-4 py-3">Action</th>
             </tr>
         </thead>
         <tbody class="bg-white">
-            @foreach ($mahasiswa as $mhs)
+            @foreach ($pribadi as $pri)
                 <tr class="text-gray-700">
                     <td class="px-4 py-3 border">
                         <div class="flex items-center text-sm">
                             <div class="relative w-8 h-8 rounded-full md:block">
                             </div>
                             <div>
-                                <div class="text-ms text-black">{{ $mhs->id_pribadi }} </div>
+                                <div class="text-ms text-black">{{ $pri->id_pribadi }} </div>
                             </div>
                         </div>
                     </td>
-                    <td class="px-4 py-3 text-ms border">{{ $mhs->nim }} </td>
-                    <td class="px-4 py-3 text-ms border">{{ $mhs->nama_mhs }} </td>
-                    @if (empty($mhs->nm_fakultas && $mhs->nm_progdi))
-                        <td class="px-4 py-3 text-ms border">{{ 'Register First' }} </td>
-                    @else
-                        <td class="px-4 py-3 text-ms border">{{ $mhs->nm_fakultas }}, {{ $mhs->nm_progdi }} </td>
-                    @endif
+                    <td class="px-4 py-3 text-ms border">{{ $pri->nik }} </td>
+                    <td class="px-4 py-3 text-ms border">{{ $pri->nama_mhs }} </td>
+                    <td class="px-4 py-3 text-ms border">{{ $pri->tempat_lahir }}, {{ $pri->tanggal_lahir }} </td>
                     <td class="px-4 py-3 text-sm border">
-                        @if (empty($mhs->nim))
-                            <a href="/mahasiswa/join/{{ $mhs->id_pribadi }}"
+                        @if (empty($pri->nim))
+                            <a href="/mahasiswa/join/{{ $pri->id_pribadi }}"
                                 class="text-white hover:text-gray-400 bg-neutral-700 rounded p-2">Not Registered</a>
                         @else
                             <button type="submit"
@@ -46,5 +42,4 @@
             @endforeach
         </tbody>
     </x-layout-table>
-    {{ $mahasiswa->links() }}
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,19 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\PribadiController;
 use App\Http\Controllers\ProgdiController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home.index');
 });
 
+Route::resource('home', HomeController::class);
 Route::resource('progdi', ProgdiController::class);
 Route::resource('pribadi', PribadiController::class);
+
 Route::resource('mahasiswa', MahasiswaController::class);
+Route::get('/search',[MahasiswaController::class, 'search'])->name('search');
+Route::get('mahasiswa/join/{id}','App\Http\Controllers\MahasiswaController@join' );
