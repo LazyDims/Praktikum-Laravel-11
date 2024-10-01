082c11ecdcb8e6022ea727579bc6589879aac059
DimasArjuna <arjunadimas832@gmail.com>
2024-10-01 18:46:18 +0700

    Handling Error

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,33 +5,5 @@
     <div style="width: 80%; margin:auto">
         <canvas id="barChart"></canvas>
     </div>
-    <script>
-        var ctx = document.getElementById('barChart').getContext('2d');
-        var myChart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: @json($data['labels']),
-                datasets: [{
-                    label: 'Jenis Data ',
-                    data: @json($data['data']),
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255,99,132,1)',
-                        'rgba(54, 162, 235, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
-    </script>
+
 </x-layout>
